242315b59ebb303535ca39aabec19ed92f77f191
Qhiba <akbarhida@gmail.com>
2023-02-17 23:45:42 +0700

    Added Enemy and Obstacle Object.

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
