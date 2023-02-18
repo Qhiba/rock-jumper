f652140a94752981e6620b66a29bb5fb64117cab
Qhiba <akbarhida@gmail.com>
2023-02-18 23:02:21 +0700

    Add simple behavior script for Enemy and Obstacle.

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -5,19 +5,44 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private float speed;
+    [SerializeField] private float playerMaxDistance;
 
     private Rigidbody2D rb2d;
+    private Transform player;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (CheckPlayerDistance() <= playerMaxDistance)
+        {
+            FollowPlayer();
+        }
     }
 
+    private float? CheckPlayerDistance()
+    {
+        float distance = 0;
+
+        if (player == null)
+        {
+            Debug.Log("There's no Player!");
+            return null;
+        }
+
+        distance = Vector2.Distance(transform.position, player.position);
+        return distance;
+    }
+
+    private void FollowPlayer()
+    {
+        Vector3 direction = (player.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+    }
 }
--- Assets/Scripts/Obstacle/Obstacle.cs
+++ Assets/Scripts/Obstacle/Obstacle.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField] private float emergeDelay = 1.0f;
+    [SerializeField] private float vanishDelay = 1.0f;
+
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        StartCoroutine(VanishCount());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator EmergeCount()
     {
-        
+        yield return new WaitForSeconds(emergeDelay);
+        spriteRenderer.enabled = true;
+        boxCollider.enabled = true;
+        StartCoroutine(VanishCount());
+    }
+
+    private IEnumerator VanishCount()
+    {
+        yield return new WaitForSeconds(vanishDelay);
+        spriteRenderer.enabled = false;
+        boxCollider.enabled = false;
+        StartCoroutine(EmergeCount());
     }
 }
